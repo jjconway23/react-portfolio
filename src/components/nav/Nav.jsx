579a9e64eb1759fef7d0dd77b0e88f3285b93d62
Jacob <jacob.peat01@gmail.com>
2022-05-30 18:25:43 +0100

    set up state to control active class. Add onclick to each link to change active class

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {AiFillHome} from 'react-icons/ai'
 import {SiAboutdotme} from 'react-icons/si'
 import {BiBook} from 'react-icons/bi'
@@ -7,13 +7,14 @@ import {AiFillMessage} from 'react-icons/ai'
 
 import "./nav.css";
 const Nav = () => {
+  const [activeClass , setActiveClass] = useState("#")
   return (
     <nav className='flex'>
-      <a href="#" className='active'><AiFillHome /></a>
-      <a href="#about"><SiAboutdotme /></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#portfolio"><BsLaptopFill /></a>
-      <a href="#contact"><AiFillMessage /></a>
+      <a href="#" onClick={()=> setActiveClass("#")} className={activeClass === "#" ? "active" : ""}><AiFillHome /></a>
+      <a href="#about" onClick={()=> setActiveClass("about")} className={activeClass === "about" ? "active" : ""}><SiAboutdotme /></a>
+      <a href="#experience" onClick={()=> setActiveClass("experience")} className={activeClass === "experience" ? "active" : ""}><BiBook/></a>
+      <a href="#portfolio" onClick={()=> setActiveClass("portfolio")} className={activeClass === "portfolio" ? "active" : ""}><BsLaptopFill /></a>
+      <a href="#contact" onClick={()=> setActiveClass("contact")} className={activeClass === "contact" ? "active" : ""}><AiFillMessage /></a>
 
     </nav>
   )
