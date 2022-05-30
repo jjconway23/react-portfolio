06b8d30c954e6b76e35230d606ff5b72eb3ee876
Jacob <jacob.peat01@gmail.com>
2022-05-30 20:08:35 +0100

    Add html markup for the portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import "./portfolio.css";
+import hangman from "../../assets/hangman.png"
+import SportsQuiz from "../../assets/SportsQuiz.png"
 const Portfolio = () => {
   return (
-    <section id="portfolio"></section>
+    <section id="portfolio">
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+
+        <div className="container portfolio__container">
+          <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={hangman} alt="hangman-pic" />
+            </div>
+            <h3>Hangman</h3>
+            <a href="https://github.com/jjconway23/Hangman" className='btn'>Github</a>
+            <a href="https://hangman-101.herokuapp.com/" className='btn btn-primary' target="_blank">Live Website</a>
+          </article>
+          <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={SportsQuiz} alt="Sports Quiz Website" />
+            </div>
+            <h3>Sports Quiz</h3>
+            <a href="https://github.com/jjconway23/sports-quiz" className='btn'>Github</a>
+            <a href="https://jjconway23.github.io/sports-quiz/" className='btn btn-primary' target="_blank">Live Website</a>
+          </article>
+          <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src="/" alt="" />
+            </div>
+            <h3>Portfolio Title goes here</h3>
+            <a href="https://github.com/jjconway23/Hangman" className='btn'>Github</a>
+            <a href="https://hangman-101.herokuapp.com/" className='btn btn-primary' target="_blank">Live Website</a>
+          </article>
+
+        </div>
+
+    </section>
   )
 }
 
