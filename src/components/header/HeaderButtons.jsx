9756c2bd9adfd7c5b1838ccf5510c74dff20cc7e
Jacob <jacob.peat01@gmail.com>
2022-05-30 16:38:06 +0100

    Style buttons within header

--- src/components/header/HeaderButtons.jsx
+++ src/components/header/HeaderButtons.jsx
@@ -3,8 +3,8 @@ import CV from "../../assets/Jacob Peat - CV.pdf"
 const HeaderButtons = () => {
   return (
     <div className="cta">
-        <a href={CV}>Download CV</a>
-        <a href="#contact">Let's talk</a>
+        <a href={CV} className="btn">Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Let's talk</a>
     </div>
   )
 }
