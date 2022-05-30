23ddeb09a4000b427fc46254f9009617b3f16758
Jacob <jacob.peat01@gmail.com>
2022-05-30 17:24:00 +0100

    Style call to action class container

--- src/components/header/HeaderButtons.jsx
+++ src/components/header/HeaderButtons.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CV from "../../assets/Jacob Peat - CV.pdf"
 const HeaderButtons = () => {
   return (
-    <div className="cta">
+    <div className="cta flex">
         <a href={CV} className="btn">Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's talk</a>
     </div>
