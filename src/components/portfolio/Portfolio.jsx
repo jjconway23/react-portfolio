46f1c0d87b37d4078d4d5bc1a2a517389a4fc248
Jacob <jacob.peat01@gmail.com>
2022-05-30 20:50:05 +0100

    refactor code and make projects section dynamic

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,47 +1,29 @@
 import React from 'react';
 import "./portfolio.css";
-import hangman from "../../assets/hangman.png"
-import SportsQuiz from "../../assets/SportsQuiz.PNG"
+import portfolioData from '../portfolioData.js';
+
 const Portfolio = () => {
+  const portfolioProjects = portfolioData.map(data => {
+    return <article key={data.id} className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={data.image} alt={`${data.title}-pic`} />
+            </div>
+            <h3>{data.title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={data.github} className='btn'>Github</a>
+              <a href={data.demo} className='btn btn-primary' target="_blank">Live Website</a>
+            </div>
+            
+    </article>
+  })
+
   return (
     <section id="portfolio">
         <h5>My Recent Work</h5>
         <h2>Portfolio</h2>
 
         <div className="container portfolio__container">
-          <article className='portfolio__item'>
-            <div className="portfolio__item-image">
-              <img src={hangman} alt="hangman-pic" />
-            </div>
-            <h3>Hangman</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/jjconway23/Hangman" className='btn'>Github</a>
-              <a href="https://hangman-101.herokuapp.com/" className='btn btn-primary' target="_blank">Live Website</a>
-            </div>
-            
-          </article>
-          <article className='portfolio__item'>
-            <div className="portfolio__item-image">
-              <img src={SportsQuiz} alt="Sports Quiz Website" />
-            </div>
-            <h3>Sports Quiz</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/jjconway23/sports-quiz" className='btn'>Github</a>
-              <a href="https://jjconway23.github.io/sports-quiz/" className='btn btn-primary' target="_blank">Live Website</a>
-            </div>
-            
-          </article>
-          <article className='portfolio__item'>
-            <div className="portfolio__item-image">
-              <img src="/" alt="" />
-            </div>
-            <h3>Portfolio Title goes here</h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/jjconway23/Hangman" className='btn'>Github</a>
-              <a href="https://hangman-101.herokuapp.com/" className='btn btn-primary' target="_blank">Live Website</a>
-            </div>
-          </article>
-
+            {portfolioProjects}
         </div>
 
     </section>
---
+++ src/components/portfolioData.js
@@ -0,0 +1,30 @@
+import hangman from "../assets/hangman.png"
+import SportsQuiz from "../assets/SportsQuiz.PNG"
+
+const portfolioData = [
+    {
+        id:1,
+        image:hangman,
+        title:"Hangman 101",
+        github: "https://github.com/jjconway23/Hangman" ,
+        demo: "https://hangman-101.herokuapp.com/" 
+    },
+    {
+        id:2,
+        image: SportsQuiz,
+        title:"Sports Quiz",
+        github: "https://github.com/jjconway23/sports-quiz"  ,
+        demo: "https://jjconway23.github.io/sports-quiz/"
+    },
+    {
+        id:3,
+        image:hangman,
+        title:"Hangman 101",
+        github: "https://github.com/jjconway23/Hangman" ,
+        demo: "https://hangman-101.herokuapp.com/" 
+    }
+    
+]
+
+
+export default portfolioData 
