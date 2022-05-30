5ec27f4efcbd895b630612f740ce98aff3c7572e
Jacob <jacob.peat01@gmail.com>
2022-05-30 18:30:32 +0100

    Begin styling the about me section. Add basic html structure

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./about.css";
-
+import ME from "../../assets/ME.jpg"
 const About = () => {
   return (
-    <section id="about"></section>
+    <section id="about">
+        <h5>Get To Know</h5>
+        <h2>About Me</h2>
+        <div className="container">
+          <div className="about__me">
+            <div className="about_me-image">
+              <img src={ME} alt="About Image" />
+            </div>
+
+          </div>
+          <div className="about__content">
+
+
+          </div>
+        </div>
+
+    </section>
   )
 }
 
