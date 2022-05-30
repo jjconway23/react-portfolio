55c4753d4507dce2804457eb51da6fb1435e6213
Jacob <jacob.peat01@gmail.com>
2022-05-30 18:02:40 +0100

    add sections to each component that will be rendered.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {AiFillHome} from 'react-icons/ai'
+import {SiAboutdotme} from 'react-icons/si'
+import {BiBook} from 'react-icons/bi'
+import {BsLaptopFill} from 'react-icons/bs'
+import {AiFillMessage} from 'react-icons/ai'
+
 import "./nav.css";
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiFillHome /></a>
+      <a href="#about"><SiAboutdotme /></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><BsLaptopFill /></a>
+      <a href="#contact"><AiFillMessage /></a>
+
+    </nav>
   )
 }
 
