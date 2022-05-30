cff6961c5802830f5e0da2178e3fa38a31734da4
Jacob <jacob.peat01@gmail.com>
2022-05-30 15:44:05 +0100

    Create Components for each section and render them to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import React from "react";
+import Header from "./components/header/Header"
+import Nav from "./components/nav/Nav"
+import About from "./components/about/About"
+import Experience from "./components/experience/Experience"
+import Portfolio from "./components/portfolio/Portfolio"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
+
+
+
+
 function App(){
     return(
-        <>Hello</>
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience/>
+            <Portfolio />
+            <Contact />
+            <Footer />
+        </>
     )
 }
 
