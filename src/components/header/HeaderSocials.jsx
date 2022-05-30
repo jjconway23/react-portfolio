6c29469060a3dd09cd75cdd29bc70b51b99aff6e
Jacob <jacob.peat01@gmail.com>
2022-05-30 17:27:13 +0100

    Style the header social links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,7 +3,7 @@ import {AiFillLinkedin} from "react-icons/ai"
 import {BsGithub} from "react-icons/bs"
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
+    <div className='header__socials flex'>
         <a href="https://www.linkedin.com/in/jacob-peat-7b524417a/" target="_blank"><AiFillLinkedin /></a>
         <a href="https://github.com/jjconway23" target="_blank"><BsGithub /></a>
         {/* <a href="" target="_blank"></a> */}
