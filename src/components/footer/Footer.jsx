9f2344a7e570292fb30c061318826a4753845e2d
Jacob <jacob.peat01@gmail.com>
2022-05-31 11:14:01 +0100

    add footer markup

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 import "./footer.css"
 const Footer = () => {
   return (
-    <footer></footer>
+    <footer>
+        <a href="#" className="footer__logo">
+
+
+        </a>
+        <ul className="permalinks">
+          <li><a href="#">Home</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div className="footer__copyright">
+            <small>&copy; Jacob Peat. All rights reserved</small>
+        </div>
+    </footer>
   )
 }
 
